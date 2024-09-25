ea0ca01c00dbcff25541f34e8f7a27d278ce2853
Jardel Felipe Knirsch <jardelfelipeknirsch@gmail.com>
2024-09-24 21:58:05 -0300

    Adicionado exemplo de listagem de itens do dicion

---
+++ dicionarios/exemplo-03.py
@@ -0,0 +1,13 @@
+def main(): 
+  dicionario = {
+    'Maça': 3,
+    'Banana': 7,
+    'Abacaxi': 1,
+    'Abacate': 2
+  }
+
+  for chave in dicionario:
+    print(f"> {chave}: {dicionario[chave]}")
+    # print(chave, dicionario[chave])
+
+main()
